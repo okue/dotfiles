9926ee68ca3c05745bc863e068e8e867a33e9733
okue <nogideca@gmail.com>
2018-11-16 20:45:37 +0900

    improve dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-ln -s ~/dotfiles/.vim          ~/.vim
-rm ~/.vim/.vim
-
-ln -s ~/dotfiles/.vimrc        ~/.vimrc
-ln -s ~/dotfiles/.inputrc      ~/.inputrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bash-completion-files ~/.bash-completion-files
-rm ~/.bash-completion-files/.bash-completion-files
+ln -s ~/dotfiles/.vim                   ~
+ln -s ~/dotfiles/.vimrc                 ~
+ln -s ~/dotfiles/.inputrc               ~
+ln -s ~/dotfiles/.bash_profile          ~
+ln -s ~/dotfiles/.bash-completion-files ~
