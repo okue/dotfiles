620823996bb4a8208d1a5a1b8973d8c57f39c0ed
okue <nogideca@gmail.com>
2019-04-13 13:00:31 +0900

    add

--- prepare_powerline_shell.sh
+++ prepare_powerline_shell.sh
@@ -2,5 +2,5 @@
 
 POWERLINEGO=~/.go/src/github.com/justjanne/powerline-go/
 
-go get -u https://github.com/justjanne/powerline-go
-cd $POWERLINEGO && git apply ~/dotfiles/diff.patch
+go get -u github.com/justjanne/powerline-go
+cd $POWERLINEGO && git apply ~/dotfiles/diff.patch && go install
