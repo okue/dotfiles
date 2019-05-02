fea4c442de1d8c161d2c05fbe791075445cac475
okue <nogideca@gmail.com>
2019-05-02 21:10:33 +0900

    add comment for kotlin-lsp

--- prepare_kotlin_lsp.sh
+++ prepare_kotlin_lsp.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+# XXX:
+# KotlinLanguageServer does not support Java12 yet.
+
 git clone https://github.com/fwcd/KotlinLanguageServer
 cd ./KotlinLanguageServer && ./gradlew server:build
