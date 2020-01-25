3b61ee7f78a75a32c4faec2ff37240d3b9369684
okue <nogideca@gmail.com>
2020-01-26 00:48:11 +0900

    remove un-used file

--- prepare_kotlin_lsp.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# XXX:
-# KotlinLanguageServer does not support Java12 yet.
-
-git clone https://github.com/fwcd/KotlinLanguageServer
-cd ./KotlinLanguageServer && ./gradlew server:build
