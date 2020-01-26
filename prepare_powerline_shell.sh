6f28b993e7183ed34f498a7d3100ae429516b001
okue <nogideca@gmail.com>
2020-01-26 14:12:03 +0900

    remove un-necessary files

--- prepare_powerline_shell.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-POWERLINEGO=~/.go/src/github.com/justjanne/powerline-go/
-
-go get -u github.com/justjanne/powerline-go
-cd $POWERLINEGO && git apply ~/dotfiles/diff.patch && go install
